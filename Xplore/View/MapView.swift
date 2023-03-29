f8588185cbab3c47f80d04ba8dfacc0216659975
Kevin Massol <kevin.massol@icloud.com>
2023-03-29 14:54:24 +0200

    [feat #13] : add different views

---
+++ Xplore/View/MapView.swift
@@ -0,0 +1,27 @@
+//
+//  MapView.swift
+//  Xplore
+//
+//  Created by KEVIN on 22/03/2023.
+//
+
+import SwiftUI
+import _MapKit_SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                PickerView()
+            }
+            .navigationTitle("Activités")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
